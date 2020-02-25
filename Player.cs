4c1d1649ad089bb4693ac904de9fca8e5443c30f
rudph1 <rudph1@ac.uk>
2020-02-25 09:51:50 +0000

    processing CSV file

---
+++ Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Debut { get; set; }
+        public int Matches { get; set; }
+        public double Average { get; set; }
+
+
+        //this is parsing of CSV file into Car object
+        internal static Player ParseFromCsv(string line)
+        {
+            var columns = line.Split(',');
+            return new Player
+            {
+                Name = columns[0],
+                Country = columns[1],
+                Debut = int.Parse(columns[2]),
+                Matches = int.Parse(columns[3]),
+                Average = double.Parse(columns[4])
+
+            };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Processing a file
+            var players = ProcessFile("player.csv");
+            var query = players.OrderBy(p => p.Debut);
+            foreach (var player in query)
+            {
+                Console.WriteLine($" {player.Name}\t{player.Debut}");
+            }
+        }
+        
+        private static List<Player> ProcessFile(string path)
+        {
+            
+            var query = from line in File.ReadAllLines(path).Skip(1)
+                        where line.Length > 1
+                        select Player.ParseFromCsv(line);
+
+                                return query.ToList();
+            
+        }
+
+        
+    }
+}
