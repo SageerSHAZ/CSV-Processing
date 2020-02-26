cdf97822d89cc138fe3a3956cdfb2bd035df4069
rudph1 <rudph1@ac.uk>
2020-02-26 12:43:31 +0000

    csv data parsing

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProcessCars("fuel.csv");
+            var manufacturers = ProcessManufacturers("manufacturers.csv");
+            
+            #region Projection 
+            var query =
+               from car in cars
+               where car.Manufacturer == "BMW" && car.Year == 2016
+               orderby car.Combined descending, car.Name ascending
+               select new
+               {
+                   car.Manufacturer,
+                   car.Name,
+                   car.Combined
+               };
+            
+            
+            foreach (var car in query.Take(10))
+                {
+                    Console.WriteLine($"{car.Manufacturer} {car.Name} : {car.Combined}");
+                }
+            #endregion
+        }
+
+        private static List<Car> ProcessCars(string path)
+        {
+            var query =
+
+                File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(l => l.Length > 1)
+                    //.ToCar();
+                    .Select(l =>
+                    {
+                        var columns = l.Split(',');
+                        return new Car
+                        {
+                            Year = int.Parse(columns[0]),
+                            Manufacturer = columns[1],
+                            Name = columns[2],
+                            Displacement = double.Parse(columns[3]),
+                            Cylinders = int.Parse(columns[4]),
+                            City = int.Parse(columns[5]),
+                            Highway = int.Parse(columns[6]),
+                            Combined = int.Parse(columns[7])
+                        };
+                    });
+
+            return query.ToList();
+        }
+
+        private static List<Manufacturer> ProcessManufacturers(string path)
+        {
+            var query =
+                   File.ReadAllLines(path)
+                       .Where(l => l.Length > 1)
+                       .Select(l =>
+                       {
+                           var columns = l.Split(',');
+                           return new Manufacturer
+                           {
+                               Name = columns[0],
+                               Headquarters = columns[1],
+                               Year = int.Parse(columns[2])
+                           };
+                       });
+            return query.ToList();
+        }
+    }
+}
